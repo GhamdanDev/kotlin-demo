352d6c87acb38ebf69c245fece2ee7c55285b316
GhamdanDev <Ghamdan.gov@gmail.com>
2025-03-04 00:34:26 +0300

    add ApiClient for Retrofit setup with logging interceptor

---
+++ app/src/main/java/com/ghamdandev/demo/data/api/ApiClient.kt
@@ -0,0 +1,27 @@
+package com.ghamdandev.demo.data.api
+
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val api: ApiService = retrofit.create(ApiService::class.java)
+}

cc28f3b7ce6368290fd01de0545e50e9533278e0
GhamdanDev <Ghamdan.gov@gmail.com>
2025-03-04 00:34:47 +0300

    add AppNavigation component with navigation setup and bottom navigation bar

---
+++ app/src/main/java/com/ghamdandev/demo/ui/navigation/AppNavigation.kt
@@ -0,0 +1,35 @@
+package com.ghamdandev.demo.ui.theme.ui.navigation
+
+import ClickCounterScreen
+import CreatePostScreen
+import PostsScreen
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+import com.ghamdandev.demo.SettingsScreen
+import com.ghamdandev.demo.ui.components.BottomNavigationBar
+
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("home") { ClickCounterScreen() }
+            composable("posts") { PostsScreen() }  // Changed from UsersScreen
+            composable("settings") { SettingsScreen() }
+            composable("createPost") { CreatePostScreen(onPostCreated = { navController.navigate("posts") }) }
+        }
+    }
+}

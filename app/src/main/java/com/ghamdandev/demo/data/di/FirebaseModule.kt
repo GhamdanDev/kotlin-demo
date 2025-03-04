8d587337f24d5be4f487a5a5f50f4e6e42146da4
GhamdanDev <Ghamdan.gov@gmail.com>
2025-03-05 02:49:34 +0300

    refactor FirebaseModule to provide Firestore instance with Hilt

--- app/src/main/java/com/ghamdandev/demo/data/di/FirebaseModule.kt
+++ app/src/main/java/com/ghamdandev/demo/data/di/FirebaseModule.kt
@@ -1,17 +1,17 @@
-//package com.ghamdandev.demo.data.di
-//
-//import com.ghamdandev.demo.data.api.FirebaseService
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import javax.inject.Singleton
-//
-//
-//@Module
-//    @InstallIn(SingletonComponent::class)
-//    object FirebaseModule {
-//        @Provides
-//        @Singleton
-//        fun provideFirebaseService(): FirebaseService = FirebaseService()
-//    }
+
+package com.ghamdandev.demo.data.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+}
